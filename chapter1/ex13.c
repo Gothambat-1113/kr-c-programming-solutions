1cac7d16bbec607e32bc17425506db8ded3da5c7
Gothambat-1113 <ravidu1113@outlook.com>
2026-06-17 20:48:48 +0530

    Fixed a bug in exercise 1.13

--- chapter1/ex13.c
+++ chapter1/ex13.c
@@ -1,37 +1,79 @@
 #include <stdio.h>
-#define  OUT 0 
-#define  IN  1
+#define OUT     0
+#define IN      1
+#define MAXHIST 15      /* max length of histogram bar      */
+#define MAXWORD 11      /* longest word length tracked + 1  */
 
+int main(void)
+{
+    int c, i, nc, state;
+    int len;
+    int maxvalue;
+    int ovflow;
+    int wl[MAXWORD];
 
-int main(){
+    state = OUT;
+    nc = 0;
+    ovflow = 0;
 
-int  c ,nl, nw, nc, state; 
+  for (i = 0; i < MAXWORD ; ++i)
+     wl[i] = 0; 
 
-state = OUT; 
-nl = nw = nc = 0; 
-while ((c=getchar()) != EOF){
-    ++nc; 
-if(c == '\n')
-    ++nl; 
-if(c== ' ' || c== '\n' || c== '\t')
-    state = OUT; 
+while((c = getchar()) != EOF){
+ if(c == ' ' || c == '\n' || c == '\t'){
+      state = OUT; 
+   if(nc > 0) {
+     if (nc < MAXWORD)
+         ++wl[nc];    
 
-else if (state == OUT){
-      state = IN; 
-      ++nw; 
+    else 
+        ++ovflow; 
 
+}
+
+ nc = 0; 
+}else if (state == OUT){
+    state = IN; 
+    nc = 1; 
+} else {
+   ++nc; 
 
 }
 
 
- 
+
 }
 
+ maxvalue = 0; 
+for (i = 1; i < MAXWORD ; ++i)
+    if(wl[i] > maxvalue)
+    maxvalue = wl[i]; 
+
+for(i = 1 ; i < MAXWORD ; ++i){
+  printf("%5d - %5d : " , i , wl[i]); 
+  if (wl[i] > 0){
+   if((len = wl[i] * MAXHIST / maxvalue) <=0)
+             len = 1; 
+ } else {
+   len = 0;
+}
+
+while(len > 0){
+     putchar('*'); 
+      --len; 
+
+}
+
+putchar ('\n'); 
+
+}
+
+if (ovflow > 0)
+   printf("there are %d words >= %d\n" , ovflow , MAXWORD);
 
 
 
 
-printf("%d  %d  %d \n" , nl , nw , nc); 
 
 
 
@@ -39,5 +81,6 @@ printf("%d  %d  %d \n" , nl , nw , nc);
 
 
 
-return 0; 
-} 
+
+    return 0;
+}
